0cacabfceb72c6b614576c4eaef317e2e58e17ce
minhee <56909923+minhee33@users.noreply.github.com>
2023-08-10 15:42:57 +0900

    [Fix] app error

--- scripts/face_detect.py
+++ scripts/face_detect.py
@@ -7,23 +7,26 @@ import os
 from src.insightface_detector import *
 
 
-
 def mount_face_detect_api(_: gr.Blocks, app: FastAPI):
+    # enable detection model only
+    face_app = FaceAnalysis(allowed_modules=['detection'])
+    face_app.prepare(ctx_id=0, det_size=(640, 640))
+
     @app.get("/facedetector/check", response_class=HTMLResponse)
     def get_check(request: Request):
         return "facedetector is okay"
-    
-    class InputImageModel(BaseModel):
-        input_image : str
 
-    @app.post("/facedetector/num",summary="Get face num in image", description="Get face num in image")
+    class InputImageModel(BaseModel):
+        input_image: str
+
+    @app.post("/facedetector/num", summary="Get face num in image", description="Get face num in image")
     async def post_face_num(faceinput: InputImageModel):
-        face_num = await insightface_face_num(input_image=faceinput.input_image)
+        face_num = await insightface_face_num(app=face_app, input_image=faceinput.input_image)
         return {"face_num": face_num}
 
-    @app.post("/facedetector/coordinate",summary="Get face coordinate in image", description="Get face coordinate in image")
+    @app.post("/facedetector/coordinate", summary="Get face coordinate in image", description="Get face coordinate in image")
     async def post_face_coordinate(faceinput: InputImageModel):
-        face_coordinate = await insightface_face_coordinate(input_image=faceinput.input_image)
+        face_coordinate = await insightface_face_coordinate(app=face_app, input_image=faceinput.input_image)
         return {"face_coordinate": face_coordinate}
 
 
